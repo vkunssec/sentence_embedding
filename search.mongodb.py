9076798bc66875b83fb035e82d689bdb77525194
Victor <vkunssec@gmail.com>
2024-04-26 15:20:49 -0300

    feature: search via terminal

--- search.mongodb.py
+++ search.mongodb.py
@@ -1,5 +1,7 @@
+import argparse
 import json
 import os
+import sys
 
 from dotenv import load_dotenv
 
@@ -7,36 +9,49 @@ from documents import EmbeddingDocuments
 
 load_dotenv()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-q", "--query", dest="query", help="Query string")
 
-uri = os.getenv("MONGODB_URI")
-db = os.getenv("MONGODB_DATABASE")
-col = "questions"
+def main(_):
+    args = parser.parse_args()
+    
+    uri = os.getenv("MONGODB_URI")
+    db = os.getenv("MONGODB_DATABASE")
+    col = "questions"
 
-model = os.getenv("MODEL")
+    model = os.getenv("MODEL")
 
-documents = EmbeddingDocuments(uri, db, col, model)
+    documents = EmbeddingDocuments(uri, db, col, model)
 
-query = documents.embedding_string('o comentário que você quer comparar').tolist()
+    q = args.query if args.query != None else "campo que será comparado"
+    print("Comparação feito a partir da Query: ", q)
+    query = documents.embedding_string(q).tolist()
 
-pipeline = []
-pipeline.append({
-    '$vectorSearch': {
-        'queryVector': query,
-        'path': 'embedding',
-        'numCandidates': 100,
-        'limit': 5,
-        'index': 'vector_index',
-    }
-})
-pipeline.append({
-    '$project': {
-        '_id': 0,
-        'id': { '$toString': '$_id' },
-        'comment': 1,
-        # 'embedding': 1,
-    }
-})
+    pipeline = []
+    pipeline.append({
+        '$vectorSearch': {
+            'queryVector': query,
+            'path': 'embedding',
+            'numCandidates': 100,
+            'limit': 5,
+            'index': 'vector_index',
+        }
+    })
+    pipeline.append({
+        '$project': {
+            '_id': 0,
+            'id': { '$toString': '$_id' },
+            'comment': 1,
+            'score': { "$meta": "vectorSearchScore" }
+        }
+    })
 
-docs = documents.collection.aggregate(pipeline=pipeline)
-print(json.dumps(list(docs), indent=4, ensure_ascii=False))
+    docs = documents.collection.aggregate(pipeline=pipeline)
+    print(json.dumps(list(docs), indent=4, ensure_ascii=False))
+    
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
 
